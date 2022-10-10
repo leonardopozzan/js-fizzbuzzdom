fcac62b6a308f7796c1cf7d3e9ddc29ec950d4e9
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-10 16:17:17 +0200

    algoritmo finito

--- js/script.js
+++ js/script.js
@@ -8,5 +8,29 @@
 // BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel 
 //BONUS 1, a seconda che il valore inserito sia un numero,
 // un fizz, un buzz o un fizzbuzz.
+let wrap = document.querySelector("div.my-container")
 
-
+for (let i = 1; i<= 100; i++){
+    let div = document.createElement('div')
+    wrap.append(div)
+    let box = document.querySelector('div.my-container div:last-child')
+    if (i%3 == 0 && i%5 == 0){
+        box.className = 'box-3-5'
+        // box.append('FizzBuzz')
+        box.innerHTML = 'FizzBuzz'
+    }else if (i%3 == 0){
+        box.className = 'box-3'
+        // box.append('Fizz')
+        box.innerHTML = 'Fizz'
+    }
+    else if (i%5 == 0){
+        box.className = 'box-5'
+        // box.append('Buzz')
+        box.innerHTML = 'Buzz'
+    }
+    else{
+        box.className = 'box'
+        // box.append(i)
+        // box.innerHTML = 'None'
+    }
+}
